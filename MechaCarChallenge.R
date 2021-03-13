81062b2bbdcc6f1cb13b00bd6a97810763d699c5
rtippana <radhika.tippanna@gmail.com>
2021-03-12 16:11:32 -0800

    Del2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,7 +10,23 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data =Car_Data)) 
 
+#Deliverable 2: Create Visualizations for the Trip Analysis
 
+#Import and Read the CSV File as DataFrame
+Coil_Data <- read.csv('Resources/Suspension_Coil.csv')
+head(Coil_Data)
+
+#mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+Mean = mean(Coil_Data$PSI)
+Median=median(Coil_Data$PSI)
+Variance=var(Coil_Data$PSI)
+SD = sd(Coil_Data$PSI)
+
+#Total_Summary Data Frame
+total_Summary <- data.frame(Mean,Median,Variance,SD)
+
+#summary by lot
+lot_summary <- Coil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep') 
 
 
 
--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Statistics and R
 1) Vehicle_weight,spoiler_angle and AWD provide non random amount of variance. The maximum random variance was provided by ground_clearence and vehicle_length
 2) Since p-value is less than zero (5.35e-11), the slope is not equal to zero
 3) The R squared value is 71.49% which implies that roughly 72% of the time the predictions will be correct using the linear model
+
+## Summary Statistics on Suspension Coils
+
+
+
+
+## T-Tests on Suspension Coils
+
+
+
+## Study Design: MechaCar vs Competition
